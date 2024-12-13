3aa836d6306310b24282887e362a35d6a1c7b3cf
swfz <sawafuji.09@gmail.com>
2024-12-14 01:57:38 +0900

    feat: popupの見た目と機能を整えた

--- popup.tsx
+++ popup.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Storage } from "@plasmohq/storage"
 import { useStorage } from "@plasmohq/storage/hook"
 
@@ -19,48 +19,49 @@ function IndexPopup() {
   const handleStart = async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
     const res = await sendToContentScript({
-      name: "hoge",
       action: "Load",
       tabId: tab.id
     })
     console.warn(res)
   }
 
-  // slide上でサンプルコメント流せるようにする
-  const handleSampleComment = () => {
-    console.log(sampleComment);
+  const handleSampleComment = async () => {
+    await sendToBackground({name: "forwarder", body: {action: "Subscribe", comments: [sampleComment]}})
   }
 
-  // 各タブとの連携状況
   return (
-    <div className="w-64 m-1">
+    <div className="w-64 m-1 flex flex-col">
       <p>
-        Click "Start" on both the slide side and the presenter user tools side
+        Click "Start" on both the slide side and the comment list side
       </p>
+
       <button
-        className="border p-2"
+        className="m-1 p-2 rounded border border-gray-400 bg-white hover:bg-gray-100"
         onClick={() => chrome.runtime.openOptionsPage()}>
         Open Option
       </button>
 
-      <div className="m-1">
-        <div>Stream Sample Comment</div>
-        <input type="text" value={sampleComment} onChange={(e) => setSampleComment(e.target.value)} className="border"></input>
-        <button
-          className="p-2 rounded border border-gray-400 bg-white hover:bg-gray-100"
-          onClick={handleSampleComment}>
-          Sample(run only slidepage)
-        </button>
+      <div className="m-1 p-1 bg-gray-100">
+        <details className="">
+          <summary className="">
+            Check Sample Comment
+          </summary>
+          <input type="text" value={sampleComment} onChange={(e) => setSampleComment(e.target.value)} className="border rounded w-full"></input>
+          <button
+            className="my-1 w-full p-2 rounded border border-gray-400 bg-white hover:bg-gray-100"
+            onClick={handleSampleComment}>
+            Sample(run only slidepage)
+          </button>
+        </details>
+
       </div>
 
-      <div className="m-1">
-        <button className="p-2 rounded border border-gray-400 bg-white hover:bg-gray-100" onClick={handleStart}>
-          Start!
-        </button>
-      </div>
+      <button className="m-1 p-2 font-medium rounded border border-gray-400 bg-white hover:bg-gray-100" onClick={handleStart}>
+        Start!
+      </button>
 
-      <div className="m-1">
-        <p>Status</p>
+      <div className="m-1 p-1 bg-gray-100">
+        <p className="text-xl">Status</p>
         <div>Slide ready: {connectionStatus?.streamer ? '✅' : '❌'}</div>
         <div>Comment Subscribe ready: {connectionStatus?.subscriber ? '✅' : '❌'}</div>
         <div>Sakura ready: {connectionStatus?.poster ? '✅' : '❌'}</div>
